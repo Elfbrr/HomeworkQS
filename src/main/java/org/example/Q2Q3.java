11f5f0e5dece623298f55ae630e692ec69603213
Elif <elifnurbirer1994@gmail.com>
2022-12-22 20:23:20 -0800

    This is first commit by Elif

---
+++ src/main/java/org/example/Q2Q3.java
@@ -0,0 +1,25 @@
+package org.example;
+
+import java.util.Scanner;
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+public class Q2Q3 {
+
+    public static void main(String[] args) {
+            //2- Define a program to find out whether a given number is even or odd.
+            Scanner sc = new Scanner(System.in);
+            int num5;
+            System.out.println("Enter the value");
+            num5 = sc.nextInt();
+            if (num5 % 2 == 0) {
+                System.out.println(num5 + " is a even number");
+            } else {
+                System.out.println(num5 + "  is a odd number");
+            }
+
+
+        }
+    }
